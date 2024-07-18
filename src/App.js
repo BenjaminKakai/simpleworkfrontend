f7e4a4fbe94d846590d203658d9dae7e35d9e5ce
Benjamin <Benjaminkakai001@gmail.com>
2024-07-18 13:28:01 +0300

    Updated my app.js to interconnect all the components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
 // src/App.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ClientForm from './ClientForm';
 import RemoveClient from './RemoveClient';
 import ClientList from './ClientList';
 import HighQualityClients from './HighQualityClients';
 import FinalizedDeals from './FinalizedDeals';
+import PendingClients from './PendingClients';
+import axios from 'axios';
+
+const backendUrl = 'http://localhost:3000';
 
 function App() {
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    const fetchClients = async () => {
+      try {
+        const response = await axios.get(`${backendUrl}/clients`);
+        setClients(response.data);
+      } catch (error) {
+        console.error('Error fetching clients:', error);
+      }
+    };
+
+    fetchClients();
+  }, []);
+
+  const handleClientAdded = (newClient) => {
+    setClients([...clients, newClient]);
+  };
+
+  const handleClientRemoved = (clientId) => {
+    setClients(clients.filter((client) => client.id !== clientId));
+  };
+
   return (
     <div className="App">
       <h1>Client Management</h1>
-      <ClientForm />
-
-      <div>
-        <hr />
-        <RemoveClient />
-      </div>
-
-      <div>
-        <hr />
-        <ClientList />
-      </div>
-
-      <div>
-        <hr />
-        <HighQualityClients />
-      </div>
-
-      <div>
-        <hr />
-        <FinalizedDeals />
-      </div>
+      <ClientForm onClientAdded={handleClientAdded} />
+      <hr />
+      <RemoveClient onClientRemoved={handleClientRemoved} />
+      <hr />
+      <ClientList clients={clients} />
+      <hr />
+      <HighQualityClients clients={clients} />
+      <hr />
+      <FinalizedDeals clients={clients} />
+      <hr />
+      <PendingClients clients={clients} />
     </div>
   );
 }
