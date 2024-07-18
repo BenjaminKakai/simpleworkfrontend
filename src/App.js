5391318c4097699b25424589194e5f53ecfa4d9e
Benjamin <Benjaminkakai001@gmail.com>
2024-07-18 12:28:37 +0300

    Updated the app.js form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-// src/App.js
+// src/App.js (main application component)
 import React from 'react';
 import ClientForm from './ClientForm';
+import ClientList from './ClientList';
 
 function App() {
+  const handleClientAdded = (newClient) => {
+    // Logic to update client list or notify other components
+    console.log('New client added:', newClient);
+  };
+
   return (
     <div className="App">
       <h1>Client Management</h1>
-      <ClientForm />
+      <ClientForm onClientAdded={handleClientAdded} />
+      <hr />
+      <ClientList />
     </div>
   );
 }
